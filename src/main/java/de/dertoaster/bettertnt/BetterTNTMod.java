77e31d014e1886ee500ab0e00af7241032cb44f3
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-11-19 12:54:43 +0100

    rename main class

---
+++ src/main/java/de/dertoaster/bettertnt/BetterTNTMod.java
@@ -0,0 +1,51 @@
+package de.dertoaster.bettertnt;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Constants.MODID)
+public class BetterTNTMod {
+	public static final Logger LOGGER = LogUtils.getLogger();
+
+	public BetterTNTMod() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+		// Register the commonSetup method for modloading
+		modEventBus.addListener(this::commonSetup);
+
+		// Register ourselves for server and other game events we are interested in
+		MinecraftForge.EVENT_BUS.register(this);
+
+		// Register the item to a creative tab
+		modEventBus.addListener(this::addCreative);
+	}
+
+	private void commonSetup(final FMLCommonSetupEvent event) {
+		// Some common setup code
+		LOGGER.info("HELLO FROM COMMON SETUP");
+		LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+	}
+
+	// Add the example block item to the building blocks tab
+	private void addCreative(BuildCreativeModeTabContentsEvent event) {
+	}
+
+	// You can use SubscribeEvent and let the Event Bus discover methods to call
+	@SubscribeEvent
+	public void onServerStarting(ServerStartingEvent event) {
+	}
+
+}
