2b5406e6675604208e824c2c34fcc5d53821d8cb
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-11-19 12:55:04 +0100

    contact explosives

---
+++ src/main/java/de/dertoaster/bettertnt/Constants.java
@@ -0,0 +1,7 @@
+package de.dertoaster.bettertnt;
+
+public class Constants {
+	
+	public static final String MODID = "bettertnt";
+
+}
---
+++ src/main/java/de/dertoaster/bettertnt/mixin/PrimedTNTMixin.java
@@ -0,0 +1,57 @@
+package de.dertoaster.bettertnt.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.item.PrimedTnt;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+
+@Mixin(PrimedTnt.class)
+public abstract class PrimedTNTMixin extends Entity {
+	
+	@Unique
+	protected boolean isFlying = false;
+	@Unique
+	protected double vLastTick = 0D;
+	
+	@Unique
+	protected final double MIN_IMPULSE = 0.35D;
+	@Unique
+	protected final double MAX_IMPULSE_FACTOR = 10.0D;
+
+	public PrimedTNTMixin(EntityType<?> pEntityType, Level pLevel) {
+		super(pEntityType, pLevel);
+	}
+	
+	private void handleContactExplosive() {
+		if(((PrimedTnt)((Object)this)).getFuse() <= 0 || !this.isAlive() || this.isRemoved()) {
+			return;
+		}
+		
+		final double v = this.getDeltaMovement().lengthSqr();
+		final double vOld = this.vLastTick;
+		this.isFlying = this.isFlying || (v > MIN_IMPULSE);
+		if(this.isFlying) {
+			System.out.println("flying");
+			if(v < (vOld / MAX_IMPULSE_FACTOR)) {
+				this.setNoGravity(true);
+				this.setDeltaMovement(Vec3.ZERO);
+				((PrimedTnt)((Object)this)).setFuse(0);
+				System.out.println("boom");
+			}
+		}
+		this.vLastTick = v;
+	}
+
+	@Inject(at = @At("HEAD"), method = "tick()V", cancellable = true)
+	private void mxinTick(CallbackInfo ci) {
+		this.handleContactExplosive();
+	}
+
+}
